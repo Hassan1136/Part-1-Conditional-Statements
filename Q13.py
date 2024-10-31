0f2abc0df9a377ee56f0550777353722ba2c6f49
Muhammad Hassan <muhammadhassan113660@gmail.com>
2024-10-31 20:31:41 +0000

    Part-1-Conditional-Statements

---
+++ Q13.py
@@ -0,0 +1,15 @@
+
+# 13. Take two numbers and an operator (+, -, *, /) as input and perform the corresponding operation.
+
+num1 = int(input("Enter a first number: "))
+num2 = int(input("Enter a second number: "))
+operator = input("Enter an operator: ")
+
+if operator == "+" :
+    print(num1, " + ", num2, " = ", num1 + num2)
+elif operator == "-" :
+    print(num1, " - ", num2, " = ", num1 - num2)
+elif operator == "*" :
+    print(num1, " * ", num2," = ", num1 * num2)
+else:
+    print(num2, " / ", num1, " = ", num1 / num2)
